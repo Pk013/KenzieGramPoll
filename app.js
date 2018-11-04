23a1128fb4229f0ef0fe60ec3499e11e8fcadb0e
morthos <pathshan61@gmail.com>
2018-11-04 12:13:04 -0500

    .

---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+const upload = multer({
+    dest: 'public/uploads/'
+})
+const port = process.env.PORT || 3000;
+const app = express();
+const uploaded_files = [];
+
+app.set('view engine', 'pug');
+app.set('views', './public/views')
+app.use(express.static('public'));
+app.use(express.static('./public/uploads/'));
+app.use(express.json());
+
+const responseData = {
+    images: [],
+    timestamp: 0
+}
+
+app.post('/update', function(req, res) {
+    res.statusCode = 200;
+    const path = './public/uploads';
+
+    fs.readdir(path, function(err, items) {
+        responseData.images = [];
+        for (let pic in items) {
+            let filename = items[pic];
+            let modified = fs.statSync(path + "/" + filename).mtimeMs;
+            if (modified > req.body.after) {
+                responseData.images.push(filename);
+                if (modified > responseData.timestamp) {
+                    responseData.timestamp = modified
+                }
+            }
+        }
+        res.send(responseData);
+    });
+});
+
+app.get('/index', function(req, res) {
+    res.statusCode = 200;
+    const path = './public/uploads';
+
+    fs.readdir(path, function(err, items) {
+        items.splice(items.findIndex(item => item === '.DS_Store'), 1)
+        for (let pic in items) {
+            let filename = items[pic];
+            let modified = fs.statSync(path + "/" + filename).mtimeMs;
+        }
+        res.render('index', {
+            title: 'Kenziegram',
+            message: 'Kenziegram',
+            images: items,
+        });
+    });
+});
+
+app.post('/upload', upload.single('myFile'), function(req, res, next) {
+    uploaded_files.push(req.file.filename);
+    res.render('upload', {
+        title: 'Kenziegram',
+        message: 'Kenziegram',
+        image: req.file.filename
+    });
+})
+
+
+app.listen(port, function() { console.log("I am working") });
---
+++ public/index.js
@@ -0,0 +1,43 @@
+const container = document.getElementById("content");
+const uploadButton = document.getElementById("upload");
+let now = Date.now();
+let interval = 5000;
+let errors = 0;
+
+function print(arr) {
+    arr.forEach(element => {
+        let img = document.createElement('img')
+        img.src = element;
+        container.appendChild(img)
+    });
+}
+
+function fetchImages() {
+
+    const postOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            after: now,
+        })
+    }
+
+    fetch("/update", postOptions)
+        .then(response => response.json())
+        .then(data => {
+            if (data.timestamp > now) {
+                print(data.images);
+            }
+            errors = 0
+            return now = data.timestamp
+        }).catch(function(error) {
+            errors++
+            if (errors == 2) {
+                clearInterval(exicutionTimer)
+            }
+        });
+    console.log(errors)
+}
+setInterval(fetchImages, interval);
